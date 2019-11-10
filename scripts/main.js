150d6f04d2450b02275fccbf18ef7021dc0b5aea
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-11-10 12:37:56 -0800

    Refactor header

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,38 @@
-const socialContainer = document.getElementById('social-container');
-const introContents = document
-  .getElementById('intro')
-  .getElementsByClassName('content')[0];
-const introContentsCoords = introContents.getBoundingClientRect();
-
-document.addEventListener('scroll', function() {
-  const scrollTop = window.scrollY;
-  const introContentsBottom =
-    introContentsCoords.y + introContentsCoords.height;
-  console.log(scrollTop, introContentsCoords.y, introContentsCoords.height);
-
-  socialContainer.style.display =
-    scrollTop > introContentsBottom ? 'block' : 'none';
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+menuBtn.addEventListener('click', event => {
+  menu.classList.toggle('hidden');
 });
+
+// const socialContainer = document.getElementById('social-container');
+// const introContents = document
+//   .getElementById('intro')
+//   .getElementsByClassName('content')[0];
+// const introContentsCoords = introContents.getBoundingClientRect();
+
+// document.addEventListener('scroll', function() {
+//   const scrollTop = window.scrollY;
+//   const introContentsBottom =
+//     introContentsCoords.y + introContentsCoords.height;
+//   console.log(scrollTop, introContentsCoords.y, introContentsCoords.height);
+
+//   socialContainer.style.display =
+//     scrollTop > introContentsBottom ? 'block' : 'none';
+// });
+
+const employerBtns = document.querySelectorAll('button.employer-tab');
+employerBtns.forEach(btn =>
+  btn.addEventListener('click', function(event) {
+    const employer = event.target.id;
+    const allJobs = document.querySelectorAll(`div.job-description`);
+    allJobs.forEach(job => job.classList.add('hidden'));
+
+    // TODO: Mark all tabs as inactive
+    // TODO: Mark toggled tab as active
+
+    const toggledJobs = document.querySelectorAll(
+      `div.job-description.${employer}`
+    );
+    toggledJobs.forEach(job => job.classList.remove('hidden'));
+  })
+);
